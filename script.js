d1b322773c6a9302f178e0c70811a80c4b45bbbf
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-11-21 12:11:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  // Since a square has equal sides, we only need one side parameter
+  constructor(side) {
+    // Call the Rectangle constructor with side as width and height
+    super(side, side);
+  }
+
+  getPerimeter() {
+    // Perimeter of a square is 4 times the length of one side
+    return 4 * this.width;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
